5673d8edffadbe024320d8e44cba3a3e2a1c6e16
davodm <irdavidnet@gmail.com>
2024-08-20 00:23:48 +0100

    fix: explicit view shouldn't defer javascript

--- src/Views/turnstile_explicit.php
+++ src/Views/turnstile_explicit.php
@@ -1,15 +1,15 @@
 <!-- Cloudflare Captcha -->
-<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=turnstileReady" defer></script>
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=turnstileReady"></script>
 <script type="text/javascript">
-    function turnstileReady() {
-        var fields = <?php echo json_encode($fields); ?>;
-        fields.forEach(function(field) {
-            turnstile.render('#' + field.fieldName, {
-                sitekey: <?php echo htmlspecialchars($siteKey, ENT_QUOTES, 'UTF-8'); ?>,
-                'response-field-name': field.fieldName,
-                size: field.size,
-                theme: field.theme
-            });
+window.turnstileReady= function() {
+    var fields = <?php echo json_encode($fields); ?>;
+    fields.forEach(function(field) {
+        turnstile.render(`#${field.fieldName}`, {
+            sitekey: '<?php echo htmlspecialchars($siteKey, ENT_QUOTES, 'UTF-8'); ?>',
+            'response-field-name': field.fieldName,
+            size: field.size,
+            theme: field.theme
         });
-    }
+    });
+}
 </script>
