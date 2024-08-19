d7cb9fe1c9a129f50cac20cfdd6bb8d58eee4104
davodm <irdavidnet@gmail.com>
2024-08-19 12:38:07 +0100

    fix: read env in config

--- src/Config/Turnstile.php
+++ src/Config/Turnstile.php
@@ -15,5 +15,22 @@ class Turnstile extends \CodeIgniter\Config\BaseConfig
     /**
      * @var string $secretKey The secret key for Turnstile.
      */
-    public string $secretKey ;
+    public string $secretKey;
+
+    
+    /**
+     * Constructor for Turnstile configuration.
+     *
+     * Loads the site key and secret key from environment variables.
+     */
+    public function __construct()
+    {
+        $this->siteKey = getenv('turnstile.siteKey');
+        $this->secretKey = getenv('turnstile.secretKey');
+
+        // Optionally, validate the keys
+        if (empty($this->siteKey) || empty($this->secretKey)) {
+            throw new \Exception('The siteKey and secretKey parameters are required.', 4);
+        }
+    }
 }
